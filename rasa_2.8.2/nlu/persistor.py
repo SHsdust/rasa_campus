f556041911b25e157cb77902986abce90ea47c72
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 20:50:42 +0000

    Adding numeric_owner as keyword arguement

--- rasa_2.8.2/nlu/persistor.py
+++ rasa_2.8.2/nlu/persistor.py
@@ -120,7 +120,7 @@ class Persistor(abc.ABC):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, target_path)
